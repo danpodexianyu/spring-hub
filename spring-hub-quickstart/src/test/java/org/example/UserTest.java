575ad51a190a7c5faccb834f9093a7286a1a7c5d
淡泊的咸鱼 <danpodexianyu@gmail.com>
2026-01-12 20:41:03 +0800

    feat: logback integration

--- spring-hub-quickstart/src/test/java/org/example/UserTest.java
+++ spring-hub-quickstart/src/test/java/org/example/UserTest.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 class UserTest {
+
+    private static final Logger log = LoggerFactory.getLogger(UserTest.class);
 
     @Test
     void testGetUserBean() {
@@ -13,8 +17,7 @@ class UserTest {
 
         // 获取创建的对象
         User user = (User) context.getBean("user");
-        System.out.println(user);
-
+        log.info("{}", user);
         // 使用对象调用方法进行测试
         user.add();
     }
