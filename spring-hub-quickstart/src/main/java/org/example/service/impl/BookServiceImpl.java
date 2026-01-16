42e828dc3f637df8d217569da88b6b7f32d6aa99
xianyu <danpodexianyu@gmail.com>
2026-01-16 21:20:41 +0800

    feat: sample DI case

--- spring-hub-quickstart/src/main/java/org/example/service/impl/BookServiceImpl.java
+++ spring-hub-quickstart/src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,7 +1,6 @@
 package org.example.service.impl;
 
 import org.example.dao.BookDao;
-import org.example.dao.impl.BookDaoImpl;
 import org.example.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +9,15 @@ public class BookServiceImpl implements BookService {
 
     private static final Logger log = LoggerFactory.getLogger(BookServiceImpl.class);
 
-    private final BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao;
 
     @Override
     public void save() {
         log.info("book service save...");
         bookDao.save();
+    }
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
