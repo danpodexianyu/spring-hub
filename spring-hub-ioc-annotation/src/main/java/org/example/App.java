677782cd2d0834eed9894b6ec508858c5db47435
xianyu <danpodexianyu@gmail.com>
2026-01-21 17:03:33 +0800

    feat: annotation bean lifecycle

--- spring-hub-ioc-annotation/src/main/java/org/example/App.java
+++ spring-hub-ioc-annotation/src/main/java/org/example/App.java
@@ -3,16 +3,17 @@ package org.example;
 import org.example.config.SpringConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class App {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
         User user = ctx.getBean("user", User.class);
         log.info("{}", user);
+        ctx.registerShutdownHook();
     }
 }
--- spring-hub-ioc-annotation/src/main/java/org/example/User.java
+++ spring-hub-ioc-annotation/src/main/java/org/example/User.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope("prototype")
 @Component(value = "user")
 public class User {
 
@@ -11,5 +15,15 @@ public class User {
 
     public User() {
         log.info("User creating...");
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("init...");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("destroy...");
     }
 }
