f3afb15ea73bea0e488b2a1d0e97b3f1a7f79e4c
xianyu <danpodexianyu@gmail.com>
2026-01-27 20:52:40 +0800

    feat: spring aop demo

---
+++ spring-hub-aop-example/src/main/java/org/example/aop/MyAdvice.java
@@ -0,0 +1,26 @@
+package org.example.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class MyAdvice {
+
+    @Pointcut("execution(* org.example.service.*Service.*(..))")
+    private void calcDurationPoint() {}
+
+    @Around("calcDurationPoint()")
+    public Object calcDuration(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        log.info("Method: {} , execute cost: {}ms", pjp.getSignature().getName(), System.currentTimeMillis() - start);
+        return obj;
+    }
+
+}
--- spring-hub-aop-example/src/main/java/org/example/config/SpringConfig.java
+++ spring-hub-aop-example/src/main/java/org/example/config/SpringConfig.java
@@ -2,7 +2,9 @@ package org.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @Configuration
 @ComponentScan("org.example")
 public class SpringConfig {
