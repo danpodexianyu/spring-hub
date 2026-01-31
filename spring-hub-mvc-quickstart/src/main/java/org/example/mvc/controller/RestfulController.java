74c324c247786f566dbbbb9eab5fc655db08acae
xianyu <danpodexianyu@gmail.com>
2026-01-31 13:15:16 +0800

    feat: spring mvc replace annotations

--- spring-hub-mvc-quickstart/src/main/java/org/example/mvc/controller/RestfulController.java
+++ spring-hub-mvc-quickstart/src/main/java/org/example/mvc/controller/RestfulController.java
@@ -1,16 +1,16 @@
 package org.example.mvc.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
 @RequestMapping("/users")
-@ResponseBody
 public class RestfulController {
 
     @RequestMapping(method = RequestMethod.POST)
