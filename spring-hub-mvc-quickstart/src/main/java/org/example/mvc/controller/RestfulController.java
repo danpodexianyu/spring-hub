74296916f7220e2dfd07c5a03c706bbbe211bdaf
xianyu <danpodexianyu@gmail.com>
2026-01-31 13:17:13 +0800

    feat: spring mvc annotations replacement

--- spring-hub-mvc-quickstart/src/main/java/org/example/mvc/controller/RestfulController.java
+++ spring-hub-mvc-quickstart/src/main/java/org/example/mvc/controller/RestfulController.java
@@ -1,9 +1,12 @@
 package org.example.mvc.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -13,22 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class RestfulController {
 
-    @RequestMapping(method = RequestMethod.POST)
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public String create() {
         return "user:create";
     }
 
-    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    // @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    @GetMapping("/{userId}")
     public String getById(@PathVariable("userId") Long userId) {
         return "user:query";
     }
 
-    @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+    // @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{userId}")
     public String delete(@PathVariable("userId") Long userId) {
         return "user:delete";
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    // @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public String update() {
         return "user:update";
     }
