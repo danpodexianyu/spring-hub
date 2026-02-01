f07ab9bb6ffe830d87984bf288ade4b77affb96f
xianyu <danpodexianyu@gmail.com>
2026-02-01 16:01:04 +0800

    feat: session scope sharing via SessionAttributes

--- spring-hub-mvc-request-params/src/main/java/org/request/params/example/controller/SessionScopeShareController.java
+++ spring-hub-mvc-request-params/src/main/java/org/request/params/example/controller/SessionScopeShareController.java
@@ -2,15 +2,24 @@ package org.request.params.example.controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/session")
+@SessionAttributes(value = {"message"})
 public class SessionScopeShareController {
 
     @RequestMapping("/testServlet")
     public String testServlet(HttpSession session) {
         session.setAttribute("message", "Native Servlet API");
+        return "session-scope-share";
+    }
+
+    @RequestMapping("/testAnnotation")
+    public String testAnnotation(ModelMap map) {
+        map.put("message", "SessionAttributes Annotation");
         return "session-scope-share";
     }
 
