26ec6c666c49a4a27cf8bd6357d9d5fd67803507
xianyu <danpodexianyu@gmail.com>
2026-02-01 14:22:33 +0800

    feat: spring mvc get request params

---
+++ spring-hub-mvc-request-params/src/main/java/org/request/params/example/controller/IndexController.java
@@ -0,0 +1,36 @@
+package org.request.params.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String toIndex() {
+        return "index";
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("password") String password,
+                           @RequestParam("gender") String gender,
+                           @RequestParam("hobby") String[] hobbies,
+                           @RequestParam("intro") String intro) {
+        log.info("username: {}", username);
+        log.info("password: {}", password);
+        log.info("gender: {}", gender);
+        log.info("hobbies: {}", Arrays.toString(hobbies));
+        log.info("intro: {}", intro);
+        return "OK";
+    }
+
+}
