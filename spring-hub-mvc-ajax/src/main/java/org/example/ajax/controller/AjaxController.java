c15e79ca79a89654906897ec699b3f9fe0db1d9c
xianyu <danpodexianyu@gmail.com>
2026-02-03 21:00:20 +0800

    feat: receive json data and convert to entity

--- spring-hub-mvc-ajax/src/main/java/org/example/ajax/controller/AjaxController.java
+++ spring-hub-mvc-ajax/src/main/java/org/example/ajax/controller/AjaxController.java
@@ -1,10 +1,14 @@
 package org.example.ajax.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.ajax.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // @Controller
+@Slf4j
 @RestController
 public class AjaxController {
 
@@ -18,6 +22,12 @@ public class AjaxController {
     // @ResponseBody
     public User ajax() {
         return new User(1L, "张三");
+    }
+
+    @PostMapping("/users")
+    public String save(@RequestBody User user) {
+        log.info("{}", user);
+        return "ok";
     }
 
 }
--- spring-hub-mvc-ajax/src/main/java/org/example/ajax/entity/User.java
+++ spring-hub-mvc-ajax/src/main/java/org/example/ajax/entity/User.java
@@ -2,9 +2,11 @@ package org.example.ajax.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Long id;
     private String username;
